a6df5d9b6c14abb04e6c071fb0b980c839c2aa36
aversIgor <igor-korotkov@narod.ru>
2019-09-04 12:54:47 +0500

    new toolbar

--- components/Desktop/toolbar.js
+++ components/Desktop/toolbar.js
@@ -3,12 +3,92 @@ import ReactDOM from 'react-dom';
 
 import logo from '../../img/logo-w-big.png'
 
+const css_toolbar = {
+    'background': '#545454ad!important'
+}
 
+const css_projectMenu = {
+    'padding-top': '10px',
+    'background': '#54545400!important'
+}
 
 export default class ComponentToolbar extends Component {
 
     constructor(props) {
         super(props);
+        this.toolbar = null;
+    }
+
+    componentDidMount(){
+        var props = this.props;  
+        
+        console.log(this)
+
+
+        const projectMenu = { 
+            view:'menu', 
+            id: 'projectMenu', 
+            css: css_projectMenu, 
+            type:{
+                subsign:true
+            },    
+            data:[
+                {
+                    id: 'actions', value:'Проект',height:50,$css:{ "color": "#ffffff!important;" },
+                    submenu:[
+                        {id: "new", icon: "file-o", value: "Новый",config:{
+                            height:36,}
+                        },
+                        {id: "open", icon: "folder-open-o", value: "Открыть"},
+                        { $template:"Separator" },
+                        {id: "save", icon: "save", value: "Сохранить"}
+                    ]
+                }
+            ],
+            on:{
+                onMenuItemClick:function(id){
+                    if(id == 'new'){
+                        window.webix.confirm({
+                            text:"Удалить все данные и начать новый проект?", ok:"Да", cancel:"Нет",
+                            callback:(res) => {
+                                if(res){
+                                    props.newProject()
+                                }
+                            }
+                        });
+                    }
+                    if(id == 'open'){
+                        window.webix.confirm({
+                            text:"При загрузке все данные текущего проекта будут утеряны. Продолжить?", ok:"Да", cancel:"Нет",
+                            callback:(res) => {
+                                if(res){
+                                    props.openProject()
+                                }
+                            }
+                        });
+                    }
+                    if(id == 'save'){
+                        props.saveProject()
+                    }
+                }
+            },
+        };
+
+        const toolbar = { 
+            view: "toolbar", 
+            css:css_toolbar, 
+            height:50,
+            type: "clean",
+            elements: [
+                {view: "label", label: "<img class='logo' src="+logo+" />", width:180},
+                projectMenu,
+                {},
+                { view: "button", type: "icon", width: 45, css: "app_button", icon: "mdi mdi-comment",  badge:4},
+                { view: "button", type: "icon", width: 45, css: "app_button", icon: "mdi mdi-bell",  badge:10}
+            ]
+        }
+
+        this.toolbar = window.webix.ui(toolbar);
     }
 
 
@@ -16,8 +96,13 @@ export default class ComponentToolbar extends Component {
         return false;
     }
 
+    componentWillUnmount(){
+        this.toolbar.destructor();
+        this.toolbar = null;
+    }
+
 
     render() {
-        return (<div ref="root" className="toolbar" view_id="toolbar"><img className='logo' src={logo} /></div>)
+        return  null        
     }
 }
