13eea5fc86440127bc02cf8fc37fb9f348d2f63c
aversIgor <igor-korotkov@narod.ru>
2019-10-20 21:58:44 +0500

    201019

--- other/tinymce/tinymce.js
+++ other/tinymce/tinymce.js
@@ -2,87 +2,118 @@ webix.protoUI({
 	name:"tinymce-editor",
 	defaults:{
 		config:{ theme:"modern", statusbar:false },
-		barHeight:74,
 		value:""
 	},
-	$init:function(config){
+	$init:function(){		
+		this._mce_id = "webix_mce_"+(this.config.id || webix.uid());
+		this.$view.innerHTML = "<textarea id='"+this._mce_id+"' style='width:100%; height:100%'></textarea>";
 		this.$view.className += " webix_selectable";
 
-		this._waitEditor = webix.promise.defer();
-		this.$ready.push(this.render);
+		this._waitEditor = webix.promise.defer();		
+		
+		this.$ready.push(this._require_tinymce_once);
 	},
 	render:function(){
 		this._set_inner_size();
-	},
-	_init_tinymce_once:function(){
-		//set id for future usage
-		this._mce_id = "webix_mce_"+this.config.id;
-		this.$view.innerHTML = "<textarea id='"+this._mce_id+"' style='width:1px; height:1px'></textarea>";
+	},	
+	_require_tinymce_once:function(){
+		var c = this.config;
+	
+
+		if (c.cdn === false || window.tinymce){
+			this._init_tinymce_once();
+			return;
+		};
+
+    var apiKey = c.apiKey ? c.apiKey : "no-api-key";
+    var cdn = c.cdn || "https://cdn.tiny.cloud/1/"+apiKey+"/tinymce/4.9.6-54";
+    
+    cdn = "other/tinymce";
 
 		//path to tinymce codebase
-		tinyMCEPreInit = { query:"", base: webix.codebase+"tinymce", suffix:".min" };
-		webix.require("tinymce/tinymce.min.js", function(){
-			if (!tinymce.dom.Event.domLoaded){
-				//woraround event logic in tinymce
-				tinymce.dom.Event.domLoaded = true;
-				webix.html.addStyle(".mce-tinymce.mce-container{ border-width:0px !important}");
-			}
-			
-			var config = this.config.config;
+		window.tinyMCEPreInit = { 
+			query:"", 
+      base: cdn,       
+			suffix:".min" 
+		};
 
-			config.mode = "exact";
-			config.height = 300;
-			config.elements = [this._mce_id];
-			config.id = this._mce_id;
+    console.log(cdn)
+		webix.require([
+      cdn+"/tinymce.min.js"
+		])
+			.then( webix.bind(this._init_tinymce_once, this) )
+			.catch(function(e){
+				console.log(e);
+			});
+	},
+	_init_tinymce_once:function(){	
+		if (!tinymce.dom.Event.domLoaded){
+			// woraround event logic in tinymce
+			tinymce.dom.Event.domLoaded = true;			
+			webix.html.addStyle(".mce-tinymce.mce-container{ border-width:0px !important}");
+		};
+		
+		var editor_config = webix.copy(this.config.config || {});
 
-			var customsetup = config.setup;
-			config.setup = webix.bind(function(editor){
-				if(customsetup) customsetup(editor);
-				this._mce_editor_setup(editor);
-			}, this);
+		webix.extend(editor_config, {
+			selector:"#"+this._mce_id,
+			resize:false,
+			mode:"exact"
+		}, true);
 
-			tinyMCE.init(config);
+		var custom_setup = editor_config.setup;
 
+		editor_config.setup = webix.bind(function(editor){			
+			if (custom_setup) 
+				custom_setup(editor);
+			editor.on("init", webix.bind(this._mce_editor_ready, this), true);			
 		}, this);
-
-		this._init_tinymce_once = function(){};
+		
+		webix.delay(function(){
+			tinymce.init(editor_config)
+		}, this);		
 	},
-	_mce_editor_setup:function(editor){
-		editor.on("init", webix.bind(this._mce_editor_ready,this))
-	},
-	_mce_editor_ready:function(editor){
-		this._3rd_editor = tinyMCE.get(this._mce_id);
-		this._set_inner_size();
-		this._waitEditor.resolve(this._3rd_editor);
+	_mce_editor_ready:function(event){
+		if (!this._editor)
+			this._editor = event.target;
+		this._waitEditor.resolve(this._editor);
 
 		this.setValue(this.config.value);
-		if (this._focus_await)
-			this.focus();
+		this._set_inner_size();	
 	},
 	_set_inner_size:function(){
-		if (!this._3rd_editor || !this.$width) return;
-		this._3rd_editor.theme.resizeTo(this.$width, this.$height - this.config.barHeight);
+		if (this._editor){
+			this._editor.theme.resizeTo(this.$width-2, this.$height - this._get_bar_height());
+		}
+	},
+	_get_bar_height:function(){
+		var bars = this.$view.querySelectorAll(".mce-toolbar, .mce-statusbar, .mce-menubar");
+		var height = 5;
+		for (var i = 0; i < bars.length; i++){
+			height += bars[i].clientHeight;
+		};
+		return height;
 	},
 	$setSize:function(x,y){
 		if (webix.ui.view.prototype.$setSize.call(this, x, y)){
-			this._init_tinymce_once();
-			this._set_inner_size();
-		}
+			this._set_inner_size();		
+		}	
 	},
 	setValue:function(value){
 		this.config.value = value;
-		if (this._3rd_editor)
-			this._3rd_editor.setContent(value);
+		this._waitEditor.then(function(editor){
+			editor.setContent(value);
+		});
 	},
 	getValue:function(){
-		return this._3rd_editor?this._3rd_editor.getContent():this.config.value;
+		return this._editor?this._editor.getContent():this.config.value;
 	},
 	focus:function(){
-		this._focus_await = true;
-		if (this._3rd_editor)
-			this._3rd_editor.focus();
+		this._waitEditor.then(function(editor){
+			editor.focus();
+		});
 	},
-	getEditor:function(waitEditor){
-		return waitEditor?this._waitEditor:this._3rd_editor;
+	getEditor:function(wait){
+		return wait?this._waitEditor:this._editor;
 	}
 }, webix.ui.view);
